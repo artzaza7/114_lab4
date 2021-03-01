0462624b032a9c6b3e0ec64012616aae03970151
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-03-01 18:35:03 +0700

    lab4_5 ตรงถามที่บอก

---
+++ lab4_5.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    int wow[2][2]={1,2,3,4};
+    int save[2][2]={1,2,3,4};
+    scanf("%d",&number);
+    fflush(stdin);
+    int check=0;
+    char view[number][50];
+    for(int i=0;i<number;i++)
+    {
+        scanf("%s",&view[i]);
+        fflush(stdin);
+    }
+    for(int i=0;i<number;i++)
+    {
+        if(check==0)
+        {
+            for(int j=0;j<50;j++)
+            {
+                if(view[i][j]=='\0')
+                {
+                    check=1;
+                    break;
+                }
+                else
+                {
+                    if(view[i][j]=='A')
+                    {
+                        int temp=wow[0][1];
+                        wow[0][1]=wow[1][0];
+                        wow[1][0]=temp;
+                    }
+                    if(view[i][j]=='B')
+                    {
+                        int temp=wow[0][0];
+                        wow[0][0]=wow[1][1];
+                        wow[1][1]=temp;
+                    }
+                    if(view[i][j]=='C')
+                    {
+                        int temp1=wow[0][0],temp2=wow[0][1];
+                        wow[0][0]=wow[1][0];
+                        wow[0][1]=wow[1][1];
+                        wow[1][0]=temp1;
+                        wow[1][1]=temp2;
+                    }
+                    if(view[i][j]=='D')
+                    {
+                        int temp1=wow[0][0],temp2=wow[1][0];
+                        wow[0][0]=wow[0][1];
+                        wow[1][0]=wow[1][1];
+                        wow[0][1]=temp1;
+                        wow[1][1]=temp2;
+                    }
+                    if(view[i][j]=='E')
+                    {
+                        int temp1=wow[0][0],temp2=wow[0][1];
+                        wow[0][0]=wow[1][1];
+                        wow[0][1]=wow[1][0];
+                        wow[1][1]=temp1;
+                        wow[1][0]=temp2;
+                    }
+                }
+            }
+        }
+        if(check==1)
+        {
+            printf("%d\n",wow[0][0]);
+            for(int i=0;i<2;i++)
+            {
+                for(int j=0;j<2;j++)
+                {
+                    wow[i][j]=save[i][j];
+                }
+            }
+            check=0;
+            continue;
+        }
+    }
+    return 0;
+}
